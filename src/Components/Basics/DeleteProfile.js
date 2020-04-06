f0cc14e2001faae23c4c831be0bdae89ff3c2a92
Augustinas Budrys <augustinas.budrys@stud.vgtu.lt>
2020-04-06 23:59:17 +0300

    DeleteProfile popup

---
+++ src/Components/Basics/DeleteProfile.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './DeleteProfile.scss';
+import InputField from "../Basics/InputField";
+import Button from "./Button";
+
+class DeleteProfile extends React.Component {
+    handleClick = () => {
+        if(this.props.clickHandler === undefined || this.props.clickHandler === null) {
+        } else {
+            this.props.clickHandler();
+        }
+    };
+
+    render() {
+        return (
+            <div className="AreYouSure">
+                <div className={'DeletePadding'}>
+                    <label className={'DeleteText'}>Are you sure you want to  <strong>delete</strong> your profile?</label>
+                </div>
+                <div>
+                    <label className={'DeleteText'}>Enter your password to confirm</label>
+                </div>
+                <div className={'DeleteAdjustField'} >
+                    <InputField width={'22rem'} type={'password'} placeholder={'Password'}/>
+                </div>
+                <div className={'DeleteContainer'}>
+                    <Button color={'red'} text={'Delete profile'} width={'12rem'}/>
+                    <Button clickHandler={this.handleClick.bind(this)} text={'Cancel'} width={'12rem'}/>
+                </div>
+            </div>
+        );
+    };
+}
+
+export default DeleteProfile;
