04ec0f0050310ce802358cff82370689eead6182
Raminta Stebulytė <raminta.stebulyte@stud.vgtu.lt>
2020-04-12 15:47:02 +0300

    login back

--- src/Components/Forms/Login.js
+++ src/Components/Forms/Login.js
@@ -6,6 +6,7 @@ import Link from "../Basics/Link";
 import Logo from "../Basics/Logo";
 import SignupForm from "./Signup";
 import Navigation from "./Navigation";
+import axios from "axios";
 
 class LoginForm extends React.Component {
     constructor(props) {
@@ -42,8 +43,27 @@ class LoginForm extends React.Component {
 
     logIn = () => {
         console.log("Congrats, you've just loged in: " + this.state.username + " - " + this.state.password);
-        //Connect login form to backend here! (and remove console log) :)
+        this.saveData();
         this.props.stateUpdater({currentPage: (<Navigation stateUpdater={this.props.stateUpdater} />)})
+    };
+
+    saveData = () => {
+        const url='http://localhost:8080/login';
+        const fakeShit = this.getUserData();
+        axios({
+            method: 'post',
+            url: url,
+            data: fakeShit
+        })
+            .then(data=>console.log(data))
+            .catch(err=>console.log(err))
+    };
+
+    getUserData = () => {
+        return this.user = {
+            password: this.state.password,
+            username: this.state.username
+        }
     };
 
     signUp = () => {
